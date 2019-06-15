2e8829776d73653a011e36898feedba8295b8a64
AndrewHSenko <51846520+AndrewHSenko@users.noreply.github.com>
2019-06-15 01:19:16 -0700

    Update FindFactors.java

--- FindFactors.java
+++ FindFactors.java
@@ -1,16 +1,17 @@
 import java.util.*;
 import java.io.*;
 /**
- * Write a description of class FindFactors here.
+ * Input a positive whole number, and it will return all whole number factors in non-repetitive pairs (i.e 25 would return 1.0 25.0 5.0)
  *
  * @Andrew Senkowski
- * @6/13/2019
+ * @3/13/2019
  */
 public class FindFactors
 {
     public static void main(String args[])
     {
         Scanner in = new Scanner(System.in);
+        System.out.print("What number's factors would you like? Number: ");
         double s = Double.parseDouble(in.nextLine());
         ArrayList<Double> factors = new ArrayList();
         boolean factorSize = false;
